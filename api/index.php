ed419d8e41536be0c8c86a4a47679812d683476e
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-12 18:37:50 -0700

    d

--- api/index.php
+++ api/index.php
@@ -3,26 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/styles.css">
+    <link rel="stylesheet" href="/api/public/styles.css">
     <title>Will You Be My Valentine?</title>
 </head>
 <body>
     <?php include 'header.php'; ?>
     <main class="main">
         <h1>Will You Be My Valentine?</h1>
-        <img src="/public/images/gamew.JPG" alt="gameW" />
-        <img src="/public/images/couple.JPG" alt="Couple" />
-        <img src="/public/images/hot.webp" alt="Hot" />
-        <img src="/public/images/liv2.JPG" alt="l2" />
-        <img src="/public/images/wine.JPG" alt="Wine" />
-        <img src="/public/images/hot2.webp" alt="Hot2" />
+        <img src="/api/public/images/gamew.JPG" alt="gameW" />
+        <img src="/api/public/images/couple.JPG" alt="Couple" />
+        <img src="/api/public/images/hot.webp" alt="Hot" />
+        <img src="/api/public/images/liv2.JPG" alt="l2" />
+        <img src="/api/public/images/wine.JPG" alt="Wine" />
+        <img src="/api/public/images/hot2.webp" alt="Hot2" />
         <div class="content">
             <h3>Roses are red, violets are blue, brown sugar and banana bread is the sweetest, and so are you!</h3>
             <button class="valentine-button">Yes, I will be your Valentine!!!!! ❤️❤️</button>
-            <button class="valentine-button"> I won't be your Valentine.. >:_) </button>
+            <button c-button"> I won't be your Valentine.. >:_) </button>
         </div>
     </main>
     <?php include 'footer.php'; ?>
 </body>
-</html>
-
+</html>lass="valentine
